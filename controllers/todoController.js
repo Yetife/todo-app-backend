57d87bbdfa770458e2a6b5c76316f98d42f3abb1
Yetife <tifetreats21@gmail.com>
2022-06-29 18:23:47 +0100

    Added files

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -2,33 +2,56 @@ const { Todos } = require('../models/todo')
 const {v4: uuid} = require("uuid")
 
 //add todos
- exports.addTodos =
-// async (req, res) => {
-//     const newTodo = new Todos(req.body)
-//     try {
-//         const savedTodo = await newTodo.save()
-//         res.status(200).json(savedTodo);
-//     }catch (err) {
-//         res.status(500).json(err)
-//     }
-// }
-
-    async (req, res) => {
+ exports.addTodos = async (req, res) => {
     try {
-        // const {title, desc} = await req.body;
         const todos = await req.body;
-        // const newTodo = {
-        //     id: uuid(),
-        //     title,
-        //     desc
-        // }
         todos.id = uuid()
-        // Todos.push(todos);
         res.status(201).json({
             message: "Todos Created",
             todos
         })
     }catch (err) {
         res.status(500).json({message: err.message});
+    }
+}
+
+//get all todos
+exports.getTodos = async (req, res)=> {
+    try {
+        const todos = await Todos;
+        res.status(200).json({
+            message: "All todos",
+            todos: todos
+        })
+    }catch (err) {
+        res.status(500).json({message: err.message});
+    }
+}
+
+exports.updateTodo = async (req, res) => {
+    try {
+        let id = req.params.id;
+        const todos = await Todos.find((flight) => flight.id === id);
+        const todo = await req.body;
+        res.status(200).json({
+            message: "flight updated",
+           todo: todos
+        })
+    } catch (err) {
+        res.status(500).json(err);
+    }
+}
+
+exports.deleteTodo = async (req, res) => {
+    try {
+        let id = req.params.id;
+        const todo = await Todos.find((todo) => todo.id === id);
+        Todos.splice(Todos.indexOf((todo), 1))
+        res.status(200).json({
+            message: "Todo deleted",
+            todo
+        })
+    } catch (err) {
+        res.status(404).json({message: err.message})
     }
 }
--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -4,4 +4,10 @@ const controller = require('../controllers/todoController');
 
 router.post("/", controller.addTodos)
 
+router.get("/", controller.getTodos)
+
+router.put("/:id", controller.updateTodo)
+
+router.delete("/:id", controller.deleteTodo)
+
 module.exports = router;
